1e96b57f935108a5b4091b2c160f3be1aa5b0339
Vitória Almeida <vitoriakarina91@gmail.com>
2026-08-16 23:07:13 +0000

    estrutura base da classe Cliente

--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -1,5 +1,61 @@
 package model;
 
-public class Cliente {
-    
+public class Cliente  {
+    private int id;
+    private String nome;
+    private String cpf;
+    public String email;
+    private int telefone;
+
+
+public Cliente(int id, String nome, int cpf, String email, int telefone) {
+    this.id = id;
+    this.nome = nome;
+    this.cpf = cpf;
+    this.email = email;
+    this.telefone = telefone;
+}
+public Cliente() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
 }
