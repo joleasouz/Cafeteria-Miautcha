908a998402cc59ec11e6d1a5cabf3909625847af
Larissa Peters <peters2009.364@gmail.com>
2026-08-17 20:09:28 -0300

    Enum com status possiveis
    Added enum Status with static methods for each status.

--- src/model/StatusPedido.java
+++ src/model/StatusPedido.java
@@ -1,5 +1,34 @@
 package model;
 
 public class StatusPedido {
-    
+public enum Status {
+    PENDENTE,
+    PREPARANDO,
+    PRONTO,
+    ENVIADO;
+
+    public static Status pendente() {
+        return PENDENTE;
+    }
+
+    public static Status preparando() {
+        return PREPARANDO;
+    }
+
+    public static Status pronto() {
+        return PRONTO;
+    }
+
+    public static Status enviado() {
+        return ENVIADO;
+    }
 }
+}
+
+//forma de usar o enum
+/* Status status = Status.preparando();
+
+if (status == Status.PREPARANDO) {
+    bglgenericodemonstracao();
+} */
+
