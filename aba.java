409c5c057f45fc86f9e2a87ce1127f5f0e7a8de3
Larissa Peters <peters2009.364@gmail.com>
2026-08-25 10:44:55 -0300

    Add new file aba.java

---
+++ aba.java
@@ -0,0 +1,26 @@
+import javax.swing.JFrame;
+import javax.swing.JTabbedPane;
+import view.Formulario;
+import view.PainelEstoque;
+
+public class aba extends JFrame {
+    public aba() {
+       setTitle("Miaucha");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JTabbedPane abas = new JTabbedPane();
+
+        Formulario aba1 = new Formulario();
+        PainelEstoque aba2 = new PainelEstoque();
+        abas.addTab("Formulario", aba1);
+        abas.addTab("Estoque", aba2);
+
+        add(abas);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new aba();
+    }
+}
