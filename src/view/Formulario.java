213dddf50c0825637a26bf37c9cae8c37f9cc486
taleddie <eduardaalcsantos@gmail.com>
2026-09-07 12:32:59 -0300

    corrigindo erros inesperados no PainelEstoque.java

--- src/view/Formulario.java
+++ src/view/Formulario.java
@@ -140,7 +140,7 @@ public class Formulario extends JPanel implements Interface {
         listaProdutos.setLayout(new BoxLayout(listaProdutos, BoxLayout.Y_AXIS));
         listaProdutos.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
 
-        for (Produto produto : catalogo) {
+        for (ProdutoDemo produto : catalogo) {
             listaProdutos.add(criarLinhaProduto(produto));
             listaProdutos.add(Box.createRigidArea(new Dimension(0, 6)));
         }
